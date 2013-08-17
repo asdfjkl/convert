db0e3f970e72afdf7757047cc8e39b492b5cbe6f
Dominik <mrtux@gmx.net>
2013-08-17 22:07:26 +0200

    chokes on 3046.xml

--- main.c
+++ main.c
@@ -11,6 +11,68 @@
 #include <dirent.h>
 #include <cstring>
 
+// read all xml files from a directory
+// extract features from kanji, and save to
+// first argument
+// TODO: moment normalize before feature extraction
+int read_directory(kanji kjis[], char* directory) {
+
+    int i = 0;
+    DIR* dirFile = opendir(directory);
+    if (dirFile) {
+        struct dirent* hFile;
+        while ((hFile = readdir(dirFile)) != NULL) {
+            if (!strcmp(hFile->d_name, ".")) continue;
+            if (!strcmp(hFile->d_name, "..")) continue;
+
+            // in linux hidden files all start with '.'
+            if (hFile->d_name[0] == '.') continue;
+
+            // dirFile.name is the name of the file. 
+            // compare to check xml ending
+            printf("now processing: %s\n", hFile->d_name);
+            if (strstr(hFile->d_name, ".xml")) {
+                kanji temp;
+                // char test[] = "test";
+                char dir_file[80];
+                strcat(dir_file, directory);
+                strcat(dir_file, hFile->d_name);
+                // printf("dirfile %s\n", dir_file);
+                read_xml_file(dir_file, &temp);
+                print_kanji(temp);
+                kanji ex = extract_features(temp, INTERVAL);
+                kjis[i] = ex;
+                i++;
+            }
+        }
+        closedir(dirFile);
+    }
+    return i;
+}
+
+// verify two sets of kanjis
+// note: need to have same number of strokes 
+// and points
+
+void verify_kanjis(kanjis kjis_a, kanji kjis_b[]) {
+
+    for (int i = 0; i < kjis_a.count; i++) {
+        for (int j = 0; j < kjis_a.arr[i].c_strokes; j++) {
+            for (int k = 0; k < kjis_a.arr[i].c_points[j]; k++) {
+                int x0 = kjis_a.arr[i].xy[j][k].x;
+                int y0 = kjis_a.arr[i].xy[j][k].y;
+                int x1 = kjis_b[i].xy[j][k].x;
+                int y1 = kjis_b[i].xy[j][k].y;
+                if ((x0 != x1) || (y0 != y1)) {
+                    printf("read-write error\n");
+                    exit(1);
+                }
+            }
+        }
+    }
+}
+
+
 int main(int argc, char **argv)
 {
         // input directory of xml files
@@ -46,67 +108,30 @@ int main(int argc, char **argv)
              abort ();
          }
         
-        if(iflag==1) {
-            kanji kjis[2200];
-            int i=0;
-                // read all xml files
-               DIR* dirFile = opendir(ivalue);
-               if ( dirFile ) 
-               {
-                  struct dirent* hFile;
-                  // int errno = 0;
-                  while (( hFile = readdir( dirFile )) != NULL ) 
-                  {
-                     if ( !strcmp( hFile->d_name, "."  )) continue;
-                     if ( !strcmp( hFile->d_name, ".." )) continue;
-
-                     // in linux hidden files all start with '.'
-                     if (hFile->d_name[0] == '.' ) continue;
-
-                     // dirFile.name is the name of the file. Do whatever string comparison 
-                     // you want here. Something like:
-                     printf("now processing: %s\n", hFile->d_name);
-                     if ( strstr( hFile->d_name, ".xml" )) {
-                         kanji temp;
-                         // char test[] = "test";
-                         char dir_file[80];
-                         strcat(dir_file,ivalue);
-                         strcat(dir_file,hFile->d_name);
-                         printf("dirfile %s\n",dir_file);
-                         read_xml_file(dir_file,&temp);
-                         print_kanji(temp);
-                         kanji ex = extract_features(temp, INTERVAL);
-                         kjis[i] = ex;
-                         i++; 
-                     }
-                  } 
-                  closedir( dirFile );
-               }
-               // write them to binary file
-               char out[] = "data.dat";
-               int cnt = i;
-               kanjis kjis_out;
-               kjis_out.count = cnt;
-               kjis_out.arr = (kanji*) malloc(cnt * sizeof(kanji));
-               for(int i=0;i<cnt;i++) {
-                   kjis_out.arr[i] = kjis[i];
-               }
-               write_bin_file(kjis_out,out);
-               kanjis kjis_bin = read_bin_file((char*) "data.dat");
-               for(int i=0;i<kjis_bin.count;i++) {
-                   for(int j=0;j<kjis_bin.arr[i].c_strokes;j++) {
-                       for(int k=0;k<kjis_bin.arr[i].c_points[j];k++) {
-                           int x0 = kjis_bin.arr[i].xy[j][k].x;
-                           int y0 = kjis_bin.arr[i].xy[j][k].y;
-                           int x1 = kjis[i].xy[j][k].x;
-                           int y1 = kjis[i].xy[j][k].y;
-                           if((x0 != x1) || (y0 != y1)) { 
-                               printf("read-write error\n");
-                               exit(1); 
-                           }
-                       }
-                   }
-               }
+        if(iflag==1 or iflag == 0) {
+            char ivalue[] = "/Users/user/Documents/Code/convert/xmls/";
+            kanji kjis[2300];
+            int cnt = read_directory(kjis,ivalue);
+            
+            // write them to binary file
+            // write_bin_file expects datatype
+            // kanjis, so first transform the array
+            char out[] = "data.dat";
+            kanjis kjis_out;
+            kjis_out.count = cnt;
+            kjis_out.arr = (kanji*) malloc(cnt * sizeof(kanji));
+            for(int i=0;i<cnt;i++) {
+                kjis_out.arr[i] = kjis[i];
+            }
+            
+            write_bin_file(kjis_out,out);
+            
+            // read them back in for verification
+            // 
+            kanjis kjis_bin = read_bin_file((char*) "data.dat");
+            verify_kanjis(kjis_bin, kjis);
+            
+            
                
         } else {
             fprintf(stderr, "Usage: convert -i XML_DIR\n");
